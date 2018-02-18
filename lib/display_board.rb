6af1dbb189ef4408f057f5edcf4b85d1263858f7
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-18 04:32:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
+def display_board(board)
   line_one ="   |   |   "
   line_two ="-----------"
   puts line_one,line_two,line_one,line_two,line_one
-  
+  puts "board#{[0]},board#{[1],board#{[2],board#{[3],board#{[4],board#{[5],board#{[6],board#{[7]"
 end
 display_board
