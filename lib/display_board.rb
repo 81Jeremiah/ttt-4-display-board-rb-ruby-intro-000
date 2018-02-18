91a55ddad25df9cc46612807260d6f44b6ea9e54
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-18 04:34:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,6 @@ def display_board(board)
   line_one ="   |   |   "
   line_two ="-----------"
   puts line_one,line_two,line_one,line_two,line_one
-  puts "board#{[0]},board#{[1],board#{[2],board#{[3],board#{[4],board#{[5],board#{[6],board#{[7]"
+  puts "board#{[0]}, board#{[1]}, board#{[2]}, board#{[3]}, board#{[4]}, board#{[5]},board#{[6]}, board#{[7]}, board#{[8]}, "
 end
 display_board
